efc079b3373a13ca404cf951dee034b81b3b7334
Andrei Dumitru <thesemeth@gmail.com>
2021-10-31 12:21:42 +0200

    changed db and link

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/devand_new/';
+$config['base_url'] = 'http://localhost/ecoart/';
 
 /*
 |--------------------------------------------------------------------------
